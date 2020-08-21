09607158c140009c163e7df3b0685c8aaedd004c
Your Name <you@example.com>
2020-08-21 15:11:24 +0800

    abc

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from config import Config
 
 app = Flask(__name__)
 app.secret_key = Config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/news_curator'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/news_curator'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b1b4046de436f6:1a31a7d6@us-cdbr-east-02.cleardb.com/heroku_3098ea224707aaa'
 app.newsapp_active_user = ""
 db = SQLAlchemy(app)
 
